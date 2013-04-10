176fea4a5e453c825d4319eb0c17c67d23e69d74
dev-yefeng <dev.yefeng@gmail.com>
2013-04-10 15:43:59 +0800

    Create Easytrip project

---
+++ Easytrip/AppConstants.h
@@ -0,0 +1,11 @@
+//
+//  AppConstants.h
+//  Easytrip
+//
+//  Created by zhang jinquan on 13-4-10.
+//  Copyright (c) 2013年 WaterDrop Studio. All rights reserved.
+//
+
+extern NSString *const kAppToken_TestFlight;
+
+extern NSString *const kAppToken_Amap;
---
+++ Easytrip/AppConstants.m
@@ -0,0 +1,15 @@
+//
+//  AppConstants.m
+//  Easytrip
+//
+//  Created by zhang jinquan on 13-4-10.
+//  Copyright (c) 2013年 WaterDrop Studio. All rights reserved.
+//
+
+#import "AppConstants.h"
+
+// Easytrip
+NSString *const kAppToken_TestFlight = @"542b3622-3329-4ab8-9b88-0529fbc80fce";
+
+// EasyTrip
+NSString *const kAppToken_Amap = @"02bb43156345e05da481de295d1d72d4";
